28183b9095135649617b99abf7e21272910cc53e
gxt568 <gxt568@cs.bham.ac.uk>
2016-02-25 12:10:07 +0000

    Created interface package

---
+++ src/interfaces/Robot.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+import java.awt.geom.Point2D;
+
+public interface Robot {
+	
+		public Point2D getPosition();
+		public Route   getRoute();
+		public ArrayList<>
+		public void    sendMessage(Message message);
+		public Message receiveMessage();
+	
+}
