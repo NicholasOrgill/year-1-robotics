3f5d7c105998215ed13282a28e98dd3ed9b5e4da
gxt568 <gxt568@cs.bham.ac.uk>
2016-02-29 14:51:36 +0000

    Started commenting assigner

--- src/ServerClasses/Assigner.java
+++ src/ServerClasses/Assigner.java
@@ -19,9 +19,13 @@ public class Assigner implements IAssigner {
 
 	/**
 	 * The default constructor for the assigner
-	 * @param _warehouse The warehouse that the assigner is based on
-	 * @param _routePlanner The route planner for the warehouse
-	 * @param _delay The delay for the thread
+	 * 
+	 * @param _warehouse
+	 *            The warehouse that the assigner is based on
+	 * @param _routePlanner
+	 *            The route planner for the warehouse
+	 * @param _delay
+	 *            The delay for the thread
 	 */
 	public Assigner(IWarehouse _warehouse, IRoutePlanner _routePlanner, int _delay) {
 		warehouse = _warehouse;
@@ -32,18 +36,28 @@ public class Assigner implements IAssigner {
 	@Override
 	public void run() {
 
+		// Whilst the warehouse is running
 		while (warehouse.getActive()) {
+			// Get the first robot
 			IRobot robot = warehouse.getRobot(0);
+			// The robot has run out of picks to collect so assign new ones
 			if (robot.getState() == RobotState.WAITING_FOR_PICKS) {
 				int weight = 0;
+				// Assign picks until the weight limit is exceeded
 				while (weight <= 50) {
+					// Get the next unassigned pick
 					IPick nextPick = warehouse.getNextIncompleteJob().getNextUnassignedPick();
+					// assign it to the robot
 					robot.assignPick(nextPick);
+					// Increase the current weight
 					weight += nextPick.getWeight();
 				}
 			}
 
+			// Once picks have been assigned the robot will be waiting for the
+			// route
 			if (robot.getState() == RobotState.WAITING_FOR_ROUTE) {
+				// Get the route from the route planner
 				IRoute route = routePlanner.getRoute(robot.getPose(), robot.getPicks().get(0).getItem().getPose());
 				robot.setRoute(route);
 			}
@@ -60,16 +74,17 @@ public class Assigner implements IAssigner {
 
 	private ArrayList<Pick> orderPicks(ArrayList<Pick> _picks) {
 
+		// Picks remaining to be assigned
 		ArrayList<Pick> remainingPicks = _picks;
+		/// Picks already assigned
 		ArrayList<Pick> selectedPicks = new ArrayList<Pick>();
 		Pick bestPick = null;
+		// While there are still picks to be assigned
 		while (remainingPicks.size() != 0) {
+			// The currently found best length of the route
 			double bestLength = Double.MAX_VALUE;
 			for (Pick selected : selectedPicks) {
 				for (Pick remaining : remainingPicks) {
-					// int length =
-					// routePlanner.getRoute(selected.getItem().getPose(),
-					// remaining.getItem().getPose()).getLength();
 					double length = routePlanner.getEuclidianDistance(selected.getItem().getPose(),
 							remaining.getItem().getPose());
 					if (length < bestLength) {
@@ -90,7 +105,7 @@ public class Assigner implements IAssigner {
 					subRoute.add(1, bestPick.getPose());
 					int newLength = routePlanner.getRoute(subRoute).getLength();
 					int diff = newLength - originalLength;
-					if(diff < minExtension){
+					if (diff < minExtension) {
 						minPos = i;
 						minExtension = diff;
 					}
@@ -102,7 +117,6 @@ public class Assigner implements IAssigner {
 			}
 		}
 		return selectedPicks;
-
 	}
 
 	@Override
