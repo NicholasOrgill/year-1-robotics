118610052d0b5717bb31ee034a094839409167c8
mac523 <mac523@cs.bham.ac.uk>
2016-02-29 16:09:58 +0000

    updated routexecutor and IRouteExecutor interface

--- src/ServerClasses/RouteExecutor.java
+++ src/ServerClasses/RouteExecutor.java
@@ -1,20 +1,52 @@
 package ServerClasses;
 
+import interfaces.IMessage;
+import interfaces.IRobot;
+import interfaces.IRouteExecuter;
 import interfaces.IWarehouse;
+import interfaces.RobotState;
 
-public class RouteExecutor implements Runnable {
+public class RouteExecutor implements IRouteExecuter {
 	
 	private IWarehouse warehouse;
+	private int delay;
+	private int robotCount;
 	
-	public RouteExecutor (IWarehouse _wh) {
+	public RouteExecutor (IWarehouse _wh, int _delay) {
 		warehouse = _wh;
+		delay = _delay;
 	}
 
 	@Override
 	public void run() {
 		while(warehouse.getActive()) {
-			
+			robotCount = warehouse.getRobotCount();
+			for(int i = 0; i < robotCount; i++) {
+				IRobot robot = warehouse.getRobot(i);
+				if (robot.getState() == RobotState.ROUTE_ASSIGNED) {
+					robot.setState(RobotState.EXECUTING_ROUTE);
+				}
+			}
 		}
 	}
 	//convert list of coordinates to list of moves 
+
+	@Override
+	public int getDelay() {
+		// TODO Auto-generated method stub
+		return delay;
+	}
+
+	@Override
+	public void setDelay(int _delay) {
+		delay = _delay;
+	}
+
+	@Override
+	public void returnMessage(IMessage _msg) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
--- src/interfaces/IRouteExecuter.java
+++ src/interfaces/IRouteExecuter.java
@@ -2,4 +2,8 @@ package interfaces;
 
 public interface IRouteExecuter extends Runnable {
 
+		public int getDelay();
+		public void setDelay(int _delay);
+		public void returnMessage(IMessage _msg);
+		
 }
