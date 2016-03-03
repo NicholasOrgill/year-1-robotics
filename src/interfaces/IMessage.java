821101dc643e0db1cc67f7eb2972a3c02047b125
gxt568 <gxt568@cs.bham.ac.uk>
2016-03-03 13:28:31 +0000

    Changed message interface, created message header enum

--- src/interfaces/IMessage.java
+++ src/interfaces/IMessage.java
@@ -2,6 +2,34 @@ package interfaces;
 
 public interface IMessage {
 
-	
-	
+	/**
+	 * Get the message header
+	 * 
+	 * @return the message header
+	 */
+	public MessageHeader getHeader();
+
+	/**
+	 * Set the message header
+	 * 
+	 * @param _header
+	 *            The message header
+	 */
+	public void setHeader(MessageHeader _header);
+
+	/**
+	 * Get the message content
+	 * 
+	 * @return The message content
+	 */
+	public String getContent();
+
+	/**
+	 * Set the message content
+	 * 
+	 * @param _content
+	 *            The new message content
+	 */
+	public void setContent(String _content);
+
 }
