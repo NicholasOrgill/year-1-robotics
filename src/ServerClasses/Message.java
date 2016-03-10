2e30a3a747589f0924221121b0a77b8b6098c875
Bede Kelly <bxk561@student.bham.ac.uk>
2016-03-10 04:37:42 +0000

    Update Message to implement IMessage correctly.

--- src/ServerClasses/Message.java
+++ src/ServerClasses/Message.java
@@ -1,24 +1,45 @@
 package ServerClasses;
 
 import interfaces.IMessage;
+import interfaces.MessageHeader;
 
 
 public class Message implements IMessage {
-    private final String body;
-    private final String header;
+    private String body;
+    private MessageHeader header;
 
-    public Message(String header, String body) {
+    public Message(MessageHeader header, String body) {
         this.header = header;
         this.body = body;
     }
 
     @Override
-    public String getHeader() {
+    public MessageHeader getHeader() {
         return header;
     }
 
+    /**
+     * Set the message header
+     *
+     * @param _header The message header
+     */
     @Override
-    public String getBody() {
+    public void setHeader(MessageHeader _header) {
+        this.header = _header;
+    }
+
+    @Override
+    public String getContent() {
         return body;
+    }
+
+    /**
+     * Set the message content
+     *
+     * @param _content The new message content
+     */
+    @Override
+    public void setContent(String _content) {
+        this.body = _content;
     }
 }
